9b4a2b65d72978991d87ad93aba2f9e37f959a27
mohammed kaif <mkaif555123@gmail.com>
2024-10-01 20:32:35 +0500

    45% Work Completed

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -5,7 +5,7 @@ function Quiz() {
     return (
         <>
 
-            <div className='bg-white p-10 w-2/4 mx-auto relative top-10 rounded-xl'>
+            <div className='bg-white p-10 w-2/4 mx-auto relative top-5 rounded-xl'>
 
                 <div>
                     <h1 className='text-2xl text-gray-800 
@@ -27,6 +27,10 @@ function Quiz() {
                 <div className='flex justify-center'>
                     <button className='bg-purple-900 text-white p-3 w-44 rounded-md
                     text-lg font-semibold'>Next</button>
+                </div>
+                <br />
+                <div>
+                    <p className='text-center'>1 To 5 questions</p>
                 </div>
             </div>
         </>
