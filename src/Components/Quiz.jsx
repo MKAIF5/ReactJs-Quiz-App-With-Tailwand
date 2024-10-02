b0fbcd4403a74d049ab57c0b038b45dfd9481e0c
mohammed kaif <mkaif555123@gmail.com>
2024-10-02 20:09:27 +0500

    85% Work Completed

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "../App.css"
 import { data } from "../assets/data.js"
 
@@ -6,14 +6,28 @@ function Quiz() {
 
     const [index, setIndex] = useState(0);
     const [questions, setQuestions] = useState(data[index]);
+    const [lock, setLock] = useState(false);
+
+
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let options_array = [Option1, Option2, Option3, Option4];
 
     const checkAns = (e, ans) => {
-        if (questions.ans === ans) {
-            e.target.classList.add("correct")
-        }
+        if (lock === false) {
+            if (questions.ans === ans) {
+                e.target.classList.add("correct");
+                setLock(true);
+            }
 
-        else {
-            e.target.classList.add("wrong")
+            else {
+                e.target.classList.add("wrong");
+                setLock(true);
+                options_array[questions.ans - 1].current.classList.add("correct");
+            }
         }
     }
 
@@ -34,10 +48,14 @@ function Quiz() {
                         {index + 1}.{questions.question}</p>
                 </div>
                 <br />
-                <div onClick={(e) => { checkAns(e, 1) }} className='border-gray-400 border p-3'>{questions.option1}</div>
-                <div onClick={(e) => { checkAns(e, 2) }} className='border-gray-400 border p-3 mt-3'>{questions.option2}</div>
-                <div onClick={(e) => { checkAns(e, 3) }} className='border-gray-400 border p-3 mt-3'>{questions.option3}</div>
-                <div onClick={(e) => { checkAns(e, 4) }} className='border-gray-400 border p-3 mt-3'>{questions.option4}</div>
+                <div ref={Option1} onClick={(e) => { checkAns(e, 1) }}
+                    className=' cursor-pointer border-gray-400 border p-3'>{questions.option1}</div>
+                <div ref={Option2} onClick={(e) => { checkAns(e, 2) }}
+                    className=' cursor-pointer border-gray-400 border p-3 mt-3'>{questions.option2}</div>
+                <div ref={Option3} onClick={(e) => { checkAns(e, 3) }}
+                    className=' cursor-pointer border-gray-400 border p-3 mt-3'>{questions.option3}</div>
+                <div ref={Option4} onClick={(e) => { checkAns(e, 4) }}
+                    className=' cursor-pointer border-gray-400 border p-3 mt-3'>{questions.option4}</div>
                 <br />
                 <div className='flex justify-center'>
                     <button className='bg-purple-900 text-white p-3 w-52 rounded-md
